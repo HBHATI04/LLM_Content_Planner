cea34185a7b177d8b510db662062451e2f1ef94b
HBHATI04 <117622958+HBHATI04@users.noreply.github.com>
2026-04-24 19:16:19 +0530

    Update analyticsRoutes.js

--- backend/routes/analyticsRoutes.js
+++ backend/routes/analyticsRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Analytics = require("../models/Analytics");
+const Analytics = require("../models/analytics");
 const User = require("../models/user");
 const authMiddleware = require("../middleware/authMiddleware");
 const adminMiddleware = require("../middleware/adminMiddleware");
