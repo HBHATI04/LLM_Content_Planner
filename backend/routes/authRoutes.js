ea0958e5b76b68d3e3b753ee53a9f6958032021a
HBHATI04 <117622958+HBHATI04@users.noreply.github.com>
2026-05-01 13:26:01 +0530

    Update authRoutes.js

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -84,7 +84,7 @@ router.post("/signup", async (req, res) => {
     // ✅ Send email
     const transporter = nodemailer.createTransport({
       host: "smtp.gmail.com",
-      port: 587,
+      port: 465,
       secure: false,
       auth: {
         user: process.env.EMAIL_USER,
